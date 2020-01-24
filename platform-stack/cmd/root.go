e8a18bbec0b73f4ef7a92815344fcb840cebe1de
Ryan A. Rodriguez <ryan.rodriguez@airbus-sv.com>
2020-01-24 10:34:11 -0800

    fixup issue with init k8s messing up root flags

--- platform-stack/cmd/root.go
+++ platform-stack/cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -157,16 +156,10 @@ func containsString(slice []string, element string) bool {
 func initK8s() {
 
 
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+	home := homeDir()
 
 	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(home, ".kube", "config"))
 	if err != nil {
 		panic(err.Error())
 	}
