3968768676c3cab054850f0bf145d6142532841f
Ryan A. Rodriguez <ryan.rodriguez@airbus-sv.com>
2020-01-28 18:26:58 -0800

    adds test case with unhealthy pod

--- platform-stack/cmd/health_test.go
+++ platform-stack/cmd/health_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 )
 
-
 //func TestHealthCLI(t *testing.T) {
 //	tests := []struct {
 //		name      string
@@ -70,6 +69,73 @@ func TestPodHealth(t *testing.T) {
 		t.Error(err.Error())
 	}
 	healthOutput := podHealth(podList)
-	golden.AssertBytes(t, []byte(healthOutput), "stack-pod-all-healthy.golden")
+	golden.AssertBytes(t, []byte(healthOutput), "stack-health-one-healthy.golden")
+
+}
+
+func TestPodHealthWithUnhealthy(t *testing.T) {
+
+	api := fake.NewSimpleClientset(&v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "tls-app-579f7cd745-t6fdg",
+			Namespace: "testns",
+			Labels: map[string]string{
+				"stack": "testapp",
+			},
+		},
+
+		Status: v1.PodStatus{
+			Phase: v1.PodRunning,
+			PodIP: "172.1.0.3",
+			ContainerStatuses: []v1.ContainerStatus{
+				{
+					State: v1.ContainerState{
+						Waiting: &v1.ContainerStateWaiting{
+							Reason:  "CrashLoopBackOff",
+							Message: "Back-off 5m0s restarting failed container=foo/bar pod=tls-app-579f7cd745-t6fdg_default(81cf37f3-3dff-11ea-b7c5-025000000001)",
+						},
+					},
+				},
+			},
+			Conditions: []v1.PodCondition{
+				{
+					Type:   "Initialized",
+					Status: v1.ConditionTrue,
+				},
+				{
+					Type:   "Ready",
+					Status: v1.ConditionFalse,
+				},
+				{
+					Type:   "ContainerReady",
+					Status: v1.ConditionFalse,
+				},
+				{
+					Type:   "PodScheduled",
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Image:           "foo/bar",
+					ImagePullPolicy: v1.PullIfNotPresent,
+				},
+			},
+			RestartPolicy: v1.RestartPolicyAlways,
+		},
+	})
+
+	podList, err := getPodsList(api.CoreV1(), "testns", "stack=testapp", "")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	healthOutput := podHealth(podList)
+	golden.AssertBytes(t, []byte(healthOutput), "stack-health-one-unhealthy.golden")
 
 }
