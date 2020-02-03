a089917f7aa85302e1054dda957c84337fd9befc
Ryan A. Rodriguez <ryan.rodriguez@airbus-sv.com>
2020-02-03 10:12:15 -0800

    adds checker, installer helper functions

--- platform-stack/cmd/install.go
+++ platform-stack/cmd/install.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -10,13 +13,38 @@ import (
 var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Installs dependencies needed to run stack commands.",
-	Long: `Installs dependencies needed to run stack commands.`,
-	RunE: runInstall,
+	Long:  `Installs dependencies needed to run stack commands.`,
+	RunE:  runInstall,
 }
 
 func runInstall(cmd *cobra.Command, args []string) (err error) {
 	fmt.Println("install called")
 	return nil
+}
+
+func dependencyExists(arg string) bool {
+	err := exec.Command("sh", "-c", arg).Run()
+
+	if exiterr, ok := err.(*exec.ExitError); ok {
+		// The program has exited with an exit code != 0
+		if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+			//return false
+			fmt.Println(status.ExitStatus())
+			return status.ExitStatus() == 0
+		}
+	}
+
+	return true
+}
+
+func installDependency(arg string) (err error) {
+	cmd := exec.Command("sh", "-c", arg)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	return err
 }
 
 func init() {
