76a0a09b59c41cca01ae4af4c7bdd85d70ede288
Sven Pedersen <sven.pedersen@airbus-sv.com>
2020-02-06 14:18:19 -0800

    Add basic sanity check with help command

--- platform-stack/cmd/logs_test.go
+++ platform-stack/cmd/logs_test.go
@@ -1,10 +1,44 @@
 package cmd
 
 import (
-	"fmt"
+	"gotest.tools/v3/golden"
+	"gotest.tools/v3/icmd"
+	"path"
 	"testing"
+	"os/exec"
 )
 
-func TestShowLogs(t *testing.T) {
-	fmt.Println("NO OP")
+
+func TestLogsCLI(t *testing.T) {
+	tests := []struct {
+		name      string
+		args      []string
+		setupArgs string
+		fixture   string
+	}{
+		{"logs help", []string{"help", "logs"}, "", "stack-logs-help.golden"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if tt.setupArgs != "" {
+				cmd := exec.Command("sh", "-c", tt.setupArgs)
+				_, err := cmd.CombinedOutput()
+				if err != nil {
+					t.Error(err)
+				}
+			}
+
+			if tt.fixture != "" {
+				cmd := exec.Command(path.Join(".", "stack"), tt.args...)
+				result, _ := cmd.CombinedOutput()
+				golden.AssertBytes(t, result, tt.fixture)
+			} else {
+				result := icmd.RunCmd(icmd.Command(path.Join(".", "stack"), tt.args...))
+				result.Assert(t, icmd.Success)
+			}
+
+		})
+	}
 }
