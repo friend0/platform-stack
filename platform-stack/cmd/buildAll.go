02e694d788d66d7b2a623f525516e665ee5c4b72
Ryan A. Rodriguez <ryan.rodriguez@airbus-sv.com>
2020-03-19 01:54:57 -0700

    fixup formatting in build all

--- platform-stack/cmd/buildAll.go
+++ platform-stack/cmd/buildAll.go
@@ -18,14 +18,22 @@ func buildAllComponents(cmd *cobra.Command, args []string) (err error) {
 		return fmt.Errorf("no components found - double check you are in a configured stack directory")
 	}
 	// todo: confirmWithUser that they are going to build multiple components, multiple containers with the same tag
-	for _, component := range config.Components {
-		fmt.Printf("Building all containers for component `%v`", component.Name)
+	for i, component := range config.Components {
+		if len(component.Containers) == 0 {
+			fmt.Printf("No images to build for component `%v` - skipping\n\n", component.Name)
+			continue
+		}
+		fmt.Printf("Building all containers for component `%v`:\n", component.Name)
 		for _, container := range component.Containers {
+			fmt.Printf("Building image `%v`:\n", container.Image)
 			tag, _ := cmd.Flags().GetString("tag")
 			err := buildComponent(container.Context, container.Dockerfile, container.Image, tag)
 			if err != nil {
 				return err
 			}
+		}
+		if i < len(config.Components) - 1 {
+			fmt.Println("")
 		}
 	}
 	return nil
