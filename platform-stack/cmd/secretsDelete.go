bdc5fe961d14d55c872a250af2c7218c40318d1f
Ryan A. Rodriguez <ryan.rodriguez@airbus-sv.com>
2020-05-28 04:43:13 -0700

    implement secretDelete

---
+++ platform-stack/cmd/secretsDelete.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+const kubectlDeleteSecretTemplate = `kubectl delete secrets {{if .SecretName}} {{- .SecretName -}} {{end}} -l stack={{ .StackName}}`
+
+type KubectlDeleteSecretsRequest struct {
+	SecretName string
+	StackName  string
+}
+
+// environmentListCmd represents the environmentList command
+var secretsDeleteCmd = &cobra.Command{
+	Use:   "delete [secretType]",
+	Short: "Delete the named stock registry.",
+	Long:  `Delete the named stock registry.`,
+	Args: cobra.MaximumNArgs(1),
+	RunE:  deleteSecret,
+}
+
+func deleteSecret(cmd *cobra.Command, args []string) error {
+	request := KubectlDeleteSecretsRequest{
+		"",
+		config.Stack.Name,
+	}
+
+	fmt.Println(len(args))
+	if len(args) > 0 {
+		fmt.Println(args[0])
+		secretType, ok := secretTypesSecretNamesMap[args[0]]
+		if ok {
+			request.SecretName = secretType
+		} else {
+			return fmt.Errorf("invalid secret type specified")
+		}
+
+	} else {
+		confirmWithUser("you are about to delete all secrets for the stack")
+	}
+
+	deleteSecretsCmd, err := GenerateCommand(kubectlDeleteSecretTemplate, request)
+	if err != nil {
+		return err
+	}
+
+	// todo: remove env append if able
+	deleteSecretsCmd.Stdout = os.Stdout
+	deleteSecretsCmd.Stderr = os.Stderr
+	if err := deleteSecretsCmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	secretsCmd.AddCommand(secretsDeleteCmd)
+}
