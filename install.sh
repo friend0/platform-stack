51ee6b0251c0ae55724e629825a8e237e52d0098
Mazedur Rahman <mazedur.rahman@airbus-sv.com>
2021-03-11 18:21:31 -0800

    Allow version pinning via stack installer

--- install.sh
+++ install.sh
@@ -1,41 +1,65 @@
 #!/usr/bin/env bash
-
+# Credits: https://gist.github.com/josh-padnick/fdae42c07e648c798fc27dec2367da21
 set -e
-
-# Default tag = latest
-readonly git_tag="${1:-latest}"
-github_repo="altiscope/platform-stack"
-output_path="./stack"
-github_api=api.github.com
+default_tag=latest
+readonly git_tag="${1:-$default_tag}"
+github_repo_owner="altiscope"
+github_repo_name="platform-stack"
+output_path="/usr/local/bin/stack"
 github_oauth_token="$GIT_TOKEN"
-
 if [[ -z "$github_oauth_token" ]]; then
   printf "Error: GIT_TOKEN not set in the environment. Run 'export GIT_TOKEN=<your-git-access-token>' and retry.\n"
   exit 1
 fi
-
 release_asset_filename="stack_$([[ $OSTYPE == darwin* ]] && echo darwin || echo linux)_amd64"
+# Get the "github tag id" of this release
+if [[ "$git_tag" == "latest" ]]; then
+	github_tag_id=$(curl --silent --show-error \
+	                     --header "Authorization: token $github_oauth_token" \
+	                     --request GET \
+	                     "https://api.github.com/repos/$github_repo_owner/$github_repo_name/releases" \
+	                     | jq --raw-output ".[0].assets | map(select(.name == \"$release_asset_filename\"))[0].id")
 
-if [ "$git_tag" = "latest" ]; then
-  # Github should return the latest release first.
-  parser=".[0].assets | map(select(.name == \"$release_asset_filename\"))[0].id"
+	download_url=$(curl --silent --show-error \
+	                   --header "Authorization: token $github_oauth_token" \
+	                   --header "Accept: application/vnd.github.v3.raw" \
+	                   --location \
+	                   --request GET \
+	                   "https://api.github.com/repos/$github_repo_owner/$github_repo_name/releases" \
+	                   | jq --raw-output ".[0].assets[0] | select(.name==\"$release_asset_filename\").url")
 else
-  parser=". | map(select(.tag_name == \"git_tag\"))[0].assets | map(select(.name == \"$release_asset_filename\"))[0].id"
+	github_tag_id=$(curl --silent --show-error \
+	                     --header "Authorization: token $github_oauth_token" \
+	                     --request GET \
+	                     "https://api.github.com/repos/$github_repo_owner/$github_repo_name/releases" \
+	                     | jq --raw-output ".[] | select(.tag_name==\"$git_tag\").id")
+	download_url=$(curl --silent --show-error \
+	                   --header "Authorization: token $github_oauth_token" \
+	                   --header "Accept: application/vnd.github.v3.raw" \
+	                   --location \
+	                   --request GET \
+	                   "https://api.github.com/repos/$github_repo_owner/$github_repo_name/releases/$github_tag_id" \
+	                   | jq --raw-output ".assets[] | select(.name==\"$release_asset_filename\").url")
 fi
 
-token_check=`curl --show-error -o /dev/null -w "%{http_code}" -sL -H "Authorization: token $github_oauth_token" -H "Accept: application/vnd.github.v3.raw" https://$github_api/repos/$github_repo/releases`
-if [[ $token_check -ne 200 ]]; then
-  printf "error: failed to install stack CLI: status code %s\n" $token_check
-  exit 1
-fi
-
-assets=`curl -sL -H "Authorization: token $github_oauth_token" -H "Accept: application/vnd.github.v3.raw" https://$github_api/repos/$github_repo/releases`
-asset_id=`echo $assets | jq "$parser"`
-
-curl --show-error --header 'Accept: application/octet-stream' --location --output "$output_path" --request GET \
-https://$github_oauth_token:@$github_api/repos/$github_repo/releases/assets/$asset_id?access_token=$github_oauth_token
-
-if [[ "$?" -ne 0 ]]; then
-  printf "error: failed to install stack CLI"
+# Get GitHub's S3 redirect URL
+# Why not just curl's built-in "--location" option to auto-redirect? Because curl then wants to include all the original
+# headers we added for the GitHub request, which makes AWS complain that we're trying strange things to authenticate.
+redirect_url=$(curl --silent --show-error \
+          --header "Authorization: token $github_oauth_token" \
+          --header "Accept: application/octet-stream" \
+          --request GET \
+          --write-out "%{redirect_url}" \
+          "$download_url")
+# Finally download the actual binary
+curl --silent --show-error \
+          --header "Accept: application/octet-stream" \
+          --output "$output_path" \
+          --request GET \
+          "$redirect_url"
+if [[ "$?" -eq 0 ]]; then
+  sudo chmod +x "$output_path"
+else
+  printf "Error: failed to install stack CLI"
   exit 1
 fi
---
+++ test-installer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# TODO: Polish this later using test framework
+
+printf "Test: stack install works with specified version.\n"
+./install.sh v0.24.0
+which stack
+stack -v
+
+printf "Test: stack install works with latest version.\n"
+./install.sh
+which stack
+stack -v
